6fa15d7078e009cd1b9293db8cfa5e19fd2ce43f
skarj <skaarj.sergey@gmail.com>
2025-04-19 19:17:57 +0300

    init

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from task_manager import views
+
 
 urlpatterns = [
-    path('', admin.site.urls),
+    path('', views.index),
     path('admin/', admin.site.urls),
 ]
